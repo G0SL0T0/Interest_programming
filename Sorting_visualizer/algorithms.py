119592b375cfad95ed8586503d2af4ebe2d307ff
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-13 21:55:10 +0500

    #3

--- Sorting_visualizer/algorithms.py
+++ Sorting_visualizer/algorithms.py
@@ -1,63 +1,45 @@
-def bubble_sort(data):
+def heap_sort(data):
+    def heapify(data, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and data[i] < data[left]:
+            largest = left
+
+        if right < n and data[largest] < data[right]:
+            largest = right
+
+        if largest != i:
+            data[i], data[largest] = data[largest], data[i]
+            yield data
+            yield from heapify(data, n, largest)
+
     n = len(data)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]
-                yield data  # Возвращаем текущее состояние данных
+    for i in range(n // 2 - 1, -1, -1):
+        yield from heapify(data, n, i)
 
-def quick_sort(data):
-    def _quick_sort(data, low, high):
-        if low < high:
-            pi = partition(data, low, high)
-            yield from _quick_sort(data, low, pi - 1)
-            yield from _quick_sort(data, pi + 1, high)
-
-    def partition(data, low, high):
-        pivot = data[high]
-        i = low - 1
-        for j in range(low, high):
-            if data[j] < pivot:
-                i += 1
-                data[i], data[j] = data[j], data[i]
-                yield data
-        data[i + 1], data[high] = data[high], data[i + 1]
+    for i in range(n - 1, 0, -1):
+        data[i], data[0] = data[0], data[i]
         yield data
-        return i + 1
+        yield from heapify(data, i, 0)
 
-    yield from _quick_sort(data, 0, len(data) - 1)
+def insertion_sort(data):
+    for i in range(1, len(data)):
+        key = data[i]
+        j = i - 1
+        while j >= 0 and key < data[j]:
+            data[j + 1] = data[j]
+            j -= 1
+            yield data
+        data[j + 1] = key
+        yield data
 
-def merge_sort(data):
-    def _merge_sort(data):
-        if len(data) > 1:
-            mid = len(data) // 2
-            left = data[:mid]
-            right = data[mid:]
-
-            yield from _merge_sort(left)
-            yield from _merge_sort(right)
-
-            i = j = k = 0
-            while i < len(left) and j < len(right):
-                if left[i] < right[j]:
-                    data[k] = left[i]
-                    i += 1
-                else:
-                    data[k] = right[j]
-                    j += 1
-                k += 1
-                yield data
-
-            while i < len(left):
-                data[k] = left[i]
-                i += 1
-                k += 1
-                yield data
-
-            while j < len(right):
-                data[k] = right[j]
-                j += 1
-                k += 1
-                yield data
-
-    yield from _merge_sort(data)
+def selection_sort(data):
+    for i in range(len(data)):
+        min_idx = i
+        for j in range(i + 1, len(data)):
+            if data[j] < data[min_idx]:
+                min_idx = j
+        data[i], data[min_idx] = data[min_idx], data[i]
+        yield data
--- Sorting_visualizer/visualization.py
+++ Sorting_visualizer/visualization.py
@@ -1,20 +1,18 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
 def visualize_sorting(data_generator, title, interval=100):
     fig, ax = plt.subplots()
     ax.set_title(title)
 
-    # Инициализация столбцов
     data = next(data_generator)
     bars = ax.bar(range(len(data)), data, color='skyblue')
 
-    # Функция обновления графика
     def update_fig(data):
         for bar, val in zip(bars, data):
             bar.set_height(val)
+            if hasattr(data_generator, 'highlight') and bar.get_x() in data_generator.highlight:
+                bar.set_color('red')
+            else:
+                bar.set_color('skyblue')
         return bars
 
-    # Создание анимации
     anim = animation.FuncAnimation(fig, update_fig, frames=data_generator, repeat=False, interval=interval)
     plt.show()
