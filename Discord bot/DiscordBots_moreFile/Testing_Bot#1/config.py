4fc362ad2e63c841854271082d6479144c550b92
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-21 06:29:02 +0500

    #conf_file

---
+++ Discord bot/DiscordBots_moreFile/Testing_Bot#1/config.py
@@ -0,0 +1,18 @@
+# Токен бота
+TOKEN = "="
+
+# Настройки для youtube
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'extract_flat': True,
+}
