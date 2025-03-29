570b1d4efc45bcbcdb03b2db96479c20338b3786
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 21:49:46 +0500

    Update reddit_fetcher.py

--- MoodScope/data_fetchers/reddit_fetcher.py
+++ MoodScope/data_fetchers/reddit_fetcher.py
@@ -1,14 +1,13 @@
 import praw
 from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT
 
-# Аутентификация
 reddit = praw.Reddit(
     client_id=REDDIT_CLIENT_ID,
     client_secret=REDDIT_CLIENT_SECRET,
     user_agent=REDDIT_USER_AGENT
 )
 
-# Сбор постов
+# Сбор
 def fetch_reddit_posts(query, count=10):
     posts = reddit.subreddit('all').search(query, limit=count)
     return [post.title for post in posts]
