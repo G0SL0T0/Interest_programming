cc20756fbdd485f901f1b7995a54ecd714f9eb92
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 21:49:21 +0500

    Update telegram_notifications.py

--- MoodScope/notifications/telegram_notifications.py
+++ MoodScope/notifications/telegram_notifications.py
@@ -3,10 +3,8 @@ from dotenv import load_dotenv
 import os
 from utils.logging_utils import log_info, log_error
 
-# Загружаем переменные окружения
 load_dotenv()
 
-# Отправка сообщения в Telegram
 def send_telegram_message(message):
     try:
         url = f"https://api.telegram.org/bot{os.getenv('token')}/sendMessage"
