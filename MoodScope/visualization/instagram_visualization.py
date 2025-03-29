7593fad1f3ff936232329fb237abc5a12819c7d5
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 21:47:31 +0500

    Update instagram_visualization.py

--- MoodScope/visualization/instagram_visualization.py
+++ MoodScope/visualization/instagram_visualization.py
@@ -1,7 +1,6 @@
 import plotly.express as px
 import pandas as pd
 
-# Визуализация данных из Instagram
 def visualize_instagram_sentiments(sentiments):
     df = pd.DataFrame(sentiments, columns=['sentiment'])
     fig = px.line(df, y='sentiment', title='Instagram Sentiment Analysis')
