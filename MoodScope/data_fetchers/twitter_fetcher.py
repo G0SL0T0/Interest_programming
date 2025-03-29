92d80c148968455e62b0d7d9703d8f9f6cbca4d5
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 21:50:00 +0500

    Update twitter_fetcher.py

--- MoodScope/data_fetchers/twitter_fetcher.py
+++ MoodScope/data_fetchers/twitter_fetcher.py
@@ -3,15 +3,12 @@ from dotenv import load_dotenv
 import os
 from utils.logging_utils import log_info, log_error
 
-# Загружаем переменные окружения
 load_dotenv()
 
-# Аутентификация
 auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_SECRET'))
 auth.set_access_token(os.getenv('access_token'), os.getenv('access_token_SECRET'))
 api = tweepy.API(auth)
 
-# Сбор твитов
 def fetch_tweets(query, count=10):
     try:
         tweets = tweepy.Cursor(api.search_tweets, q=query, lang="en").items(count)
