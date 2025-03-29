771719d6930ff315cf89dafa039f433d18fec509
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 21:47:14 +0500

    Update database.py

--- MoodScope/database.py
+++ MoodScope/database.py
@@ -3,15 +3,13 @@ from dotenv import load_dotenv
 import os
 from utils.logging_utils import log_info, log_error
 
-# Загружаем переменные окружения
 load_dotenv()
 
-# Подключение к базе данных
+# Подключение к базе
 DATABASE_PATH = os.getenv('DATABASE_PATH')
 conn = sqlite3.connect(DATABASE_PATH)
 cursor = conn.cursor()
 
-# Создание таблицы (если её нет)
 def init_db():
     try:
         cursor.execute('''CREATE TABLE IF NOT EXISTS sentiments
@@ -25,7 +23,6 @@ def init_db():
     except Exception as e:
         log_error(f"Error initializing database: {e}")
 
-# Сохранение данных
 def save_to_db(platform, text, sentiment):
     try:
         cursor.execute("INSERT INTO sentiments (platform, text, sentiment) VALUES (?, ?, ?)",
@@ -35,7 +32,6 @@ def save_to_db(platform, text, sentiment):
     except Exception as e:
         log_error(f"Error saving data to database: {e}")
 
-# Получение данных
 def fetch_data():
     try:
         cursor.execute("SELECT * FROM sentiments")
@@ -44,7 +40,6 @@ def fetch_data():
         log_error(f"Error fetching data from database: {e}")
         return []
 
-# Закрытие соединения
 def close_db():
     try:
         conn.close()
