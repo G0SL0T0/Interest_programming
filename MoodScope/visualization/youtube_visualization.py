0d65f59d3659ca5e24428bd07c398aa9818bf0ef
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 21:48:18 +0500

    Update youtube_visualization.py

--- MoodScope/visualization/youtube_visualization.py
+++ MoodScope/visualization/youtube_visualization.py
@@ -1,7 +1,6 @@
 import plotly.express as px
 import pandas as pd
 
-# Визуализация данных из YouTube
 def visualize_youtube_sentiments(sentiments):
     df = pd.DataFrame(sentiments, columns=['sentiment'])
     fig = px.line(df, y='sentiment', title='YouTube Sentiment Analysis')
