183ab482ec246396b61f4c4977d436f909a6ae6a
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-29 22:05:12 +0500

    Update youtube_fetcher.py

--- MoodScope/data_fetchers/youtube_fetcher.py
+++ MoodScope/data_fetchers/youtube_fetcher.py
@@ -1,10 +1,8 @@
 from googleapiclient.discovery import build
 from config import YOUTUBE_API_KEY
 
-# Инициализация YouTube API
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
-# Сбор комментариев под видео
 def fetch_youtube_comments(video_id, count=10):
     comments = youtube.commentThreads().list(
         part="snippet",
