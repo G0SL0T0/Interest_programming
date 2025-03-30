a0d659c1503b24df043f730daa5b637dfbb3793d
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-03-30 19:06:13 +0500

    Update trend_analysis.py

--- MoodScope/analysis/trend_analysis.py
+++ MoodScope/analysis/trend_analysis.py
@@ -1,7 +1,6 @@
 from nltk import FreqDist
 from nltk.tokenize import word_tokenize
 
-# Анализ трендов
 def analyze_trends(texts):
     words = [word for text in texts for word in word_tokenize(text.lower()) if word.isalpha()]
     freq_dist = FreqDist(words)
