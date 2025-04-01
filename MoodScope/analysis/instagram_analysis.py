9efa788952058545e7d7de6f939a32f2f319f542
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-04-01 05:46:28 +0500

    Update instagram_analysis.py

--- MoodScope/analysis/instagram_analysis.py
+++ MoodScope/analysis/instagram_analysis.py
@@ -3,7 +3,7 @@ from sentiment_analysis import analyze_sentiment
 def analyze_instagram_posts(posts):
     sentiments = []
     for post in posts:
-        if post:  # Если пост не пустой
+        if post:
             sentiment = analyze_sentiment(post)
             sentiments.append(sentiment)
     return sentiments
