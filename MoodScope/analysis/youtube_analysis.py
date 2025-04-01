08a668079393f2b31168e665b2cccd6e7e21bb6b
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-04-01 05:46:39 +0500

    Update youtube_analysis.py

--- MoodScope/analysis/youtube_analysis.py
+++ MoodScope/analysis/youtube_analysis.py
@@ -3,7 +3,7 @@ from sentiment_analysis import analyze_sentiment
 def analyze_youtube_comments(comments):
     sentiments = []
     for comment in comments:
-        if comment:  # Если комментарий не пустой
+        if comment:
             sentiment = analyze_sentiment(comment)
             sentiments.append(sentiment)
     return sentiments
